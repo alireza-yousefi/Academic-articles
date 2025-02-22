64983a9b1b38377e3ed0b7a38409aa8b2d7a34c7
Alireza <alireza.ys245@gmail.com>
2025-02-22 15:39:03 +0330

    published v-1

--- app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="Estedad_FD_Light h-full">
-    <NuxtPage class="" />
-  </div>
-</template>
-
--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  css: ['@/public/css/main.css',"@/public/css/font.css"],
-
-  modules: ['@nuxtjs/tailwindcss'],
-
-});
